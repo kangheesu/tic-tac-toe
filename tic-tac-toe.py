9b8cbcb39cc8ee3747961d38ed73eee0be396ca7
kangheesu <33407112+kangheesu@users.noreply.github.com>
2017-11-26 22:35:10 +0900

    60162091_강희수
    tic-tac-toe 과제

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter.messagebox
 
 def checked(i) :
       global player
@@ -15,7 +16,32 @@ def checked(i) :
       else :
             player = "X"
             button["bg"] = "lightgreen"
-
+            ifWin()
+    
+def ifWin():
+      if(list[0]["text"==list[1]["text"]==list[2]["text"] != "     "):
+                 win_ms()
+      if(list[3]["text"==list[4]["text"]==list[5]["text"] != "     "):
+                 win_ms()
+      if(list[6]["text"==list[7]["text"]==list[8]["text"] != "     "):
+                 win_ms()
+      if(list[0]["text"==list[3]["text"]==list[6]["text"] != "     "):
+                 win_ms()
+      if(list[1]["text"==list[4]["text"]==list[7]["text"] != "     "):
+                 win_ms()
+      if(list[2]["text"==list[5]["text"]==list[8]["text"] != "     "):
+                 win_ms()
+      if(list[0]["text"==list[4]["text"]==list[8]["text"] != "     "):
+                 win_ms()
+      if(list[2]["text"==list[4]["text"]==list[6]["text"] != "     "):
+                 win_ms()                 
+def win_ms():
+      if player == "X":
+                 tkinter.messagebox.showinfo("Winner","player O")
+                 quit()
+                 tkinter.messagebox.showinfo("Winner","player X")
+                 quit()
+   
 window = Tk()
 player = "X"
 list= []
